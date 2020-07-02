51603e596aa5dd64cb8ad9f202571f14805d18f5
8ward <8ward@chelnok.de>
2020-07-02 13:02:11 +0200

    Integrate example GraphViz and PlantUML filters

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Pandoc Filters for CodiMD
    $ pip3 install -r requirements.txt
    $ git submodule update --init
    ```
+   (may require `plantuml.jar` in the working directory)
 2. Convert Markdown file
    ```
    $ (cat <header>.yaml; ./preprocess.sh <file>.md) \
--- codimd.py
+++ codimd.py
@@ -6,7 +6,14 @@ from codimd.crossrefs import crossrefs
 from codimd.emojis import emojis
 from codimd.images import images
 
+import sys
+from os import path
+sys.path.insert(0, path.realpath(path.join(path.dirname(__file__), 'pandocfilters', 'examples')))
+from graphviz import graphviz
+from plantuml import plantuml
+del sys.path[0]
+
 
 if __name__ == '__main__':
-    toJSONFilters([ containers, crossrefs, emojis, images ])
+    toJSONFilters([ containers, crossrefs, emojis, images, graphviz, plantuml ])
 
